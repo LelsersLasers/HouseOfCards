41a23b1d7e142636602aec3d46cac1a44c921d93
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2023-09-01 21:17:10 -0700

    Making game slightly easier

--- house_of_cards/src/consts.rs
+++ house_of_cards/src/consts.rs
@@ -132,4 +132,4 @@ pub const XP_PER_LEVEL: fn(i32) -> i32 = |level| 2 * level.pow(2);
 pub const AUTO_LOG: fn(f32, f32, f32, f32) -> f32 =
     |x, a, b, c| ((b - a) / (c + 1.0).log10()) * (x + 1.0).log10() + a;
 pub const ENEMY_WAVE_SPAWN_RATE: fn(i32) -> f32 =
-    |wave| AUTO_LOG(wave as f32, 1.0 / 3.5, 1.0 / 1.5, 10.0);
+    |wave| AUTO_LOG(wave as f32, 1.0 / 5.0, 1.0 / 2.0, 10.0);
